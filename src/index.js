1587f5d30f777d76585384432acf08bb9b91fb83
Ashim Singh <ashimsingh948@gmail.com>
2022-08-31 22:02:11 +0545

    ternary operator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import {useState} from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App(){
     const [text,setText]=useState()
+    const [isError,setIsError] =useState(false)
     return(
         <>
-            <h1>{text||"hello Ashim"}</h1>
+            {/* And Or operator
+             <h1>{text||"hello Ashim"}</h1>
             {text&&<h1>You are my</h1>}
-            {!text && <h1>Sunshine </h1>}
+            {!text && <h1>Sunshine </h1>} */}
+
+
+            {/* Ternnary operator */}
+            <div class="w-50  d-flex  flex-column" style={{margin:"20%"}}>
+            <button type="button" class="btn btn-info" onClick={()=>{setIsError(!isError)}}>{isError ? "Fix Error":"Find Error"}</button>
+            {isError?<h1>Error...</h1>:<h1>Every Thing  all okay</h1>}
+            </div>
         </>
     )
 }
