c1740acb96823ff11ac19d3e9b59e39dd220553c
Ashim Singh <ashimsingh948@gmail.com>
2022-09-02 15:50:30 +0545

    handeling multiple return

--- src/show/form.js
+++ src/show/form.js
@@ -1,24 +1,40 @@
 import React, { useState } from 'react'
 import './index.css'
 const MyForm=()=>{
-    const [naam,setNaam]=useState('')
-    const [mail,setMail]=useState('')
+    // const [naam,setNaam]=useState('')
+    // const [mail,setMail]=useState('')
+    const [people,setPeople]=useState({firstName:'',email:'',age:''})
     const [person,setPerson]=useState([])
-    const handelForm=(e)=>{
+    const handelChange=(e)=>{
+        const name = e.target.name
+        const value = e.target.value
+        setPeople({...people,[name]:value})
+    }
+    const handelForm =(e)=>{
         e.preventDefault()
-        if(naam && mail){
-            const people = {id:new Date().getTime().toString(),firstName:naam,email:mail}
-            setPerson((person)=>{
-                return ([...person,people])
-            })
-            setNaam('')
-            setMail('')
-        }
-        // console.log("hello Art")
-        else{
-            console.log("empty value inserted")
+        if(people.firstName && people.email && people.age){
+            const newPerson = {...people,id:new Date().getTime().toString()}
+            setPerson(
+                [...person,newPerson]
+            )
+            setPeople({ firstName:'',email:"",age:'' })
         }
     }
+    // const handelForm=(e)=>{
+    //     e.preventDefault()
+    //     if(naam && mail){
+    //         const people = {id:new Date().getTime().toString(),firstName:naam,email:mail}
+    //         setPerson((person)=>{
+    //             return ([...person,people])
+    //         })
+    //         setNaam('')
+    //         setMail('')
+    //     }
+        // console.log("hello Art")
+    //     else{
+    //         console.log("empty value inserted")
+    //     }
+    // }
     return(
         <>
                 <form className="tForn w-50  d-flex  flex-column" style={{margin:"20%", height:"50px"}} onSubmit={handelForm}>
@@ -29,8 +45,9 @@ const MyForm=()=>{
                             id='firstName' 
                             name='firstName' 
                             placeholder='Enter your Name'
-                            value={naam}
-                            onChange={(e)=>setNaam(e.target.value)}
+                            value={people.firstName}
+                            // onChange={(e)=>setNaam(e.target.value)}
+                            onChange={handelChange}
                             ></input>
                         </label>
                     </div>
@@ -39,10 +56,24 @@ const MyForm=()=>{
                         <input 
                             type='text' 
                             id='email' 
-                            name='firstName' 
-                            placeholder='Enter your Name'
-                            value={mail}
-                            onChange={(e)=>setMail(e.target.value)}
+                            name='email' 
+                            placeholder='Enter Email'
+                            value={people.mail}
+                            // onChange={(e)=>setMail(e.target.value)}
+                            onChange={handelChange}
+                            ></input>
+                        </label>
+                    </div>
+                    <div className="p-2">
+                        <label htmlFor='age'> Age :   
+                        <input 
+                            type='text' 
+                            id='age' 
+                            name='age' 
+                            placeholder='Enter Age'
+                            value={people.age}
+                            // onChange={(e)=>setMail(e.target.value)}
+                            onChange={handelChange}
                             ></input>
                         </label>
                     </div>
@@ -51,13 +82,16 @@ const MyForm=()=>{
             {/* {naam && <h1>hello</h1>} */}
             <div className='d-flex flex-column justify-content-center'>
                 {person.map((p)=>{
-                const{id,firstName,email}=p
+                const{id,firstName,email,age}=p
                 return(
                     <>
                         <div key={id}>
-                            <h1>{p.firstName}</h1>
+                            <h1>{firstName}</h1>
                         <p>
-                            {p.email}
+                            {email}
+                        </p>
+                        <p>
+                            {age}
                         </p>
                         </div>
                     </>
