690e6f829141b0bafed7d479b318877258c351e3
Ashim Singh <ashimsingh948@gmail.com>
2022-09-05 22:19:15 +0545

    setting up useReducer

--- src/show/useReducer.js
+++ src/show/useReducer.js
@@ -1,25 +1,85 @@
+// import React from 'react'
+// import {useState} from 'react'
+// import Model from './model'
+// const Redu=()=>{
+//     const [name,setName]=useState('')
+//     const[people,setPeople]=useState([{firstName:"ashim",id:"123"},
+// {firstName:"Car",id:"456"}])
+//     const[showModel,setModel]=useState(false)
+//     const handelSubmit=(e)=>{
+//         e.preventDefault()
+//         if(name){
+//             setModel(false)
+//             const newName={firstName:name,id:new Date().getTime.toString()}
+//             setPeople((p)=>{
+//                 return([...p,newName])
+//             }
+//             )
+//             setName('')
+            
+//         }
+//         else{
+//             setModel(true)
+//         }
+//     }
+//     return(
+        
+//         <>
+//             <form onSubmit={handelSubmit}>
+//                 <div className='d-flex m-4 mb-0 flex-column '>
+//                     <label>Name:
+//                         <input 
+//                         type='text'
+//                         value={name}
+//                         onChange={(e) => setName(e.target.value)}>
+//                         </input>
+//                     </label>
+                    
+//                 </div>
+//                 <button  className="btn btn-info m-5 mt-1" type='submit'>Add Person</button>
+//             </form>
+//             {showModel && <Model/>}
+//             {
+//                 people.map((p)=>{
+//                     const {id,firstName}=p
+//                     return(
+//                         <>
+//                             <div className="d-flex m-3" key={id}>
+//                                 <h1>{firstName}</h1>
+//                             </div>
+//                         </>
+//                     )
+//                 })
+//             }
+//         </>
+//     )
+// }
+// export default Redu
+
+
+
 import React from 'react'
-import {useState} from 'react'
+import {useState,useReducer} from 'react'
 import Model from './model'
+const reducer=(action,state)=>{}
+const initialState={
+    people:[],
+    isModel:false,
+    modalContent:"Hey Ashim"
+}
 const Redu=()=>{
     const [name,setName]=useState('')
-    const[people,setPeople]=useState([{firstName:"ashim",id:"123"},
-{firstName:"Car",id:"456"}])
-    const[showModel,setModel]=useState(false)
+    // const[people,setPeople]=useState([{firstName:"ashim",id:"123"},
+    //             {firstName:"Car",id:"456"}])
+    // const[showModel,setModel]=useState(false)
+    const [state,dispatch]=useReducer(reducer,initialState)
     const handelSubmit=(e)=>{
         e.preventDefault()
         if(name){
-            setModel(false)
-            const newName={firstName:name,id:new Date().getTime.toString()}
-            setPeople((p)=>{
-                return([...p,newName])
-            }
-            )
-            setName('')
-            
+           
         }
         else{
-            setModel(true)
+            
         }
     }
     return(
@@ -38,9 +98,9 @@ const Redu=()=>{
                 </div>
                 <button  className="btn btn-info m-5 mt-1" type='submit'>Add Person</button>
             </form>
-            {showModel && <Model/>}
+            {state.isModel && <Model/>}
             {
-                people.map((p)=>{
+                state.people.map((p)=>{
                     const {id,firstName}=p
                     return(
                         <>
